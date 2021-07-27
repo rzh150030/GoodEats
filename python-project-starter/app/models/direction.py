894bbe97f8e18f8039035c28d2d29c7e773dcb97
Richard Huang <rzhuangsg115@gmail.com>
2021-07-27 11:46:08 -0500

    create relation in direction.py

--- python-project-starter/app/models/direction.py
+++ python-project-starter/app/models/direction.py
@@ -8,3 +8,5 @@ class Direction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     step = db.Column(db.String(5000), nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id', ondelete="CASCADE"), nullable=False)
+
+    recipe = db.relationship("Recipe", back_populates="directions")
