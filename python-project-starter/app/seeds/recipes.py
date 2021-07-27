f6de29a1a1d55ec788cb69f915fc33fb432c2b3c
Richard Huang <rzhuangsg115@gmail.com>
2021-07-27 14:33:40 -0500

    finish seeding recipes

---
+++ python-project-starter/app/seeds/recipes.py
@@ -0,0 +1,17 @@
+from app.models import db, Recipe
+
+
+def seed_recipes():
+    recipe1 = Recipe(name="Chicken Soup", category_id=4, user_id=1)
+    recipe2 = Recipe(name="Chezburger", category_id=4, user_id=1)
+    recipe3 = Recipe(name="Ribs", category_id=43, user_id=2)
+
+    db.session.add(recipe1)
+    db.session.add(recipe2)
+    db.session.add(recipe3)
+
+    db.session.commit()
+
+def undo_recipes():
+    db.session.execute('TRUNCATE recipes RESTART IDENTITY CASCADE;')
+    db.session.commit()
