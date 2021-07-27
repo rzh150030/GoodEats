6b4ae892e46cabdc98ae13f1fbd357a15b32a00d
Richard Huang <rzhuangsg115@gmail.com>
2021-07-27 11:47:22 -0500

    create relation in ingredient.py

--- python-project-starter/app/models/ingredient.py
+++ python-project-starter/app/models/ingredient.py
@@ -7,3 +7,5 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ingredient = db.Column(db.String(5000), nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id', ondelete="CASCADE"), nullable=False)
+
+    recipe = db.relationship("Recipe", back_populates="ingredients")
