6131473d7524a79a68be10216425d5301fa7e6e9
Richard Huang <rzhuangsg115@gmail.com>
2021-07-27 21:42:37 -0500

    start working on navbar, changing links to buttons

--- python-project-starter/react-app/src/NavBar/index.js
+++ python-project-starter/react-app/src/NavBar/index.js
@@ -1,15 +1,20 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import LogoutButton from '../components/auth/LogoutButton';
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const sessionUser = useSelector(state => state.session.user)
+
   return (
     <nav>
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            Good Eats
           </NavLink>
         </li>
         <li>
@@ -20,11 +25,6 @@ const NavBar = () => {
         <li>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
           </NavLink>
         </li>
         <li>
