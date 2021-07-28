7fdac2366c89d54d04aa7bd3dc6d511b25190935
Richard Huang <rzhuangsg115@gmail.com>
2021-07-28 12:04:29 -0500

    create submit handler

--- python-project-starter/react-app/src/components/CreateRecipe/index.js
+++ python-project-starter/react-app/src/components/CreateRecipe/index.js
@@ -7,9 +7,40 @@ export default function CreateRecipe() {
     const history = useHistory();
     const dispatch = useDispatch();
     //const categories = useSelector(state => state.recipe.categories);
-    //const [category, setCategory] = useState("")
+    //const [category, setCategory] = useState("Ainu");
+    const [name, setName] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+    const [directions, setDirections] = useState([]);
+    const [errors, setErrors] = useState([]);
+
+    //onChange event handlers
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const data = {
+            name,
+            category: 1,
+            ingredients,
+            directions
+        };
+
+        try {
+            dispatch(postRecipe(data));
+            history.push("/");
+        } catch (error) {
+            const badData = useSelector(state => state.recipes.errors)
+            setErrors(badData);
+        }
+
+    }
 
     return (
-        <h1>Insert form</h1>
+        <form>
+            <ul>
+                {errors && errors.map((err, i) => <li key={i}>{err}</li>)}
+            </ul>
+
+        </form>
     )
 }
