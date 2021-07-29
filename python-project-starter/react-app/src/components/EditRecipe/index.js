bbbd62297b81a9ceac3280eba353f612fbd32d87
Richard Huang <rzhuangsg115@gmail.com>
2021-07-29 16:53:27 -0500

    redirect user when refreshing on edit

--- python-project-starter/react-app/src/components/EditRecipe/index.js
+++ python-project-starter/react-app/src/components/EditRecipe/index.js
@@ -1,16 +1,19 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
+import React, {} from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import RecipeForm from "../RecipeForm";
 
 export default function EditRecipe() {
+    const history = useHistory();
+    const {id} = useParams();
     const currentRecipe = useSelector(state => state.recipe.currentRecipe);
     const initIngredients = currentRecipe.ingredients;
     const initDirections = currentRecipe.directions;
     const initName = currentRecipe.name;
     const checkWhichMethod = false; // prop for form component
 
-    
+    if (!currentRecipe.id) history.push(`/recipe/detail/${id}`);
+    console.log(currentRecipe)
 
     return (
         <>
