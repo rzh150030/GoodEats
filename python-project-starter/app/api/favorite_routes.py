1aa1eadb31e60198b04f1d845091c171cb520c9e
Richard Huang <rzhuangsg115@gmail.com>
2021-07-30 18:15:11 -0500

    create route to favor a recipe

--- python-project-starter/app/api/favorite_routes.py
+++ python-project-starter/app/api/favorite_routes.py
@@ -3,3 +3,10 @@ from flask_login import login_required
 from app.models import db, Category
 
 favorite_routes = Blueprint('favorites', __name__)
+
+
+# Favor a recipe
+@favorite_routes.route("/favor/<int:id>", methods=["POST"])
+# @login_required
+def favor_recipe(id):
+    
