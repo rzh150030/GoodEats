451743cacd61ab0bde5ed4b7aa41b383f936f1d2
Richard Huang <rzhuangsg115@gmail.com>
2021-07-30 18:20:50 -0500

    set up getting all user favorite

--- python-project-starter/app/api/favorite_routes.py
+++ python-project-starter/app/api/favorite_routes.py
@@ -1,3 +1,4 @@
+from logging import log
 from flask import Blueprint
 from flask_login import login_required
 from app.models import db, Recipe
@@ -5,9 +6,18 @@ from app.models import db, Recipe
 favorite_routes = Blueprint('favorites', __name__)
 
 
+# Get all user's favorite recipes
+@favorite_routes.route("/all/<int:id>")
+# @login_required
+def all_user_favorites(id):
+    
+
 # Favor a recipe
 @favorite_routes.route("/favor/<int:id>", methods=["POST"])
 # @login_required
 def favor_recipe(id):
     recipe = Recipe.query.get(id)
-    
+    if recipe.userfavs:
+        return {"message": "yes"}
+
+    return {"message": "in route, not pass condition"}
