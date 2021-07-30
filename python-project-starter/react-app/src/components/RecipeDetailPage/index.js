73702164d0eb917cf02e48af76a36f0cd86be23b
Richard Huang <rzhuangsg115@gmail.com>
2021-07-30 15:53:00 -0500

    classnames for necessary html

--- python-project-starter/react-app/src/components/RecipeDetailPage/index.js
+++ python-project-starter/react-app/src/components/RecipeDetailPage/index.js
@@ -28,7 +28,7 @@ export default function RecipeDetailPage() {
     let editDeleteButton;
     if (sessionUser && sessionUser.id === currentRecipe.user_id) {
         editDeleteButton = (
-            <div>
+            <div className="recipe-detail-buttons">
                 <button onClick={editRecipe}>Edit</button>
                 <button onClick={deletion}>Delete</button>
             </div>
@@ -36,28 +36,30 @@ export default function RecipeDetailPage() {
     }
 
     return (
-        <article className="recipe-detail-page">
+        <>
             <h1>{currentRecipe.name}</h1>
-            <div>
-                <label>Ingredients: </label>
-                <ul>
-                    {currentRecipe.ingredients?.sort(({id: a}, {id: b}) => a - b).map(ingred => (
-                        <li key={ingred.id}>{ingred.ingredient}</li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <label>Directions: </label>
-                <ol>
-                    {currentRecipe.directions?.sort(({id: a}, {id: b}) => a - b).map(direct => (
-                        <li key={direct.id}>{direct.step}</li>
-                    ))}
-                </ol>
-            </div>
-            <div>
-                {editDeleteButton}
-                <h2>Category: {currentRecipe.category_name}</h2>
-            </div>
-        </article>
+            <article className="recipe-detail-page">
+                <div>
+                    <label className="ingred-detail-label">Ingredients: </label>
+                    <ul className="ingred-detail-list">
+                        {currentRecipe.ingredients?.sort(({id: a}, {id: b}) => a - b).map(ingred => (
+                            <li key={ingred.id}>{ingred.ingredient}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <label className="direct-detail-label">Directions: </label>
+                    <ol className="direct-detail-list">
+                        {currentRecipe.directions?.sort(({id: a}, {id: b}) => a - b).map(direct => (
+                            <li key={direct.id}>{direct.step}</li>
+                        ))}
+                    </ol>
+                </div>
+                <div>
+                    {editDeleteButton}
+                    <h2 className="recipe-detail-category">Category: {currentRecipe.category_name}</h2>
+                </div>
+            </article>
+        </>
     )
 }
