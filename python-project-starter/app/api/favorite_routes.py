68c174b36750cd6d1287c3c200c8afaffb32f808
Richard Huang <rzhuangsg115@gmail.com>
2021-07-30 18:42:12 -0500

    create to_dict for user favorites in user model

--- python-project-starter/app/api/favorite_routes.py
+++ python-project-starter/app/api/favorite_routes.py
@@ -1,16 +1,17 @@
 from logging import log
 from flask import Blueprint
-from flask_login import login_required
-from app.models import db, Recipe
+from flask_login import login_required, current_user
+from app.models import db, Recipe, User
 
 favorite_routes = Blueprint('favorites', __name__)
 
 
 # Get all user's favorite recipes
-@favorite_routes.route("/all/<int:id>")
+@favorite_routes.route("/<int:id>")
 # @login_required
 def all_user_favorites(id):
-    
+    user = User.query.get(id)
+    return user.to_dict_with_favors()
 
 # Favor a recipe
 @favorite_routes.route("/favor/<int:id>", methods=["POST"])
