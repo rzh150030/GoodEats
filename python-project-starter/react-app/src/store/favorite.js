9d870f51804cb7cd495306b6d824453b0e23ef5a
Richard Huang <rzhuangsg115@gmail.com>
2021-07-31 20:22:52 -0500

    set up favorite reducer

--- python-project-starter/react-app/src/store/favorite.js
+++ python-project-starter/react-app/src/store/favorite.js
@@ -1,3 +1,14 @@
 const LOAD_FAVORITES = "favorite/loadFavorites";
 const FAVOR_RECIPE = "favorite/favorRecipe";
 const UNFAVOR_RECIPE = "favorite/unfavorRecipe";
+
+
+
+const initialState = {favorites: {}};
+
+export default function favoriteReducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
