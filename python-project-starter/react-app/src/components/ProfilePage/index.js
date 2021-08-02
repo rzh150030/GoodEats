ec5c227476af1163ac84197118fdd9cb16e2e6ef
Richard Huang <rzhuangsg115@gmail.com>
2021-08-02 12:19:41 -0500

    create profile page comp and imports

---
+++ python-project-starter/react-app/src/components/ProfilePage/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { userRecipes } from '../../store/recipe';
+import { getFavoredRecipes } from '../../store/favorite';
+
+export default function ProfilePage() {
+    
+}
