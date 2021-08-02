b5542f022c78accd2ec5f4414ccddbd4870dfd5b
Richard Huang <rzhuangsg115@gmail.com>
2021-08-02 11:29:41 -0500

    made 2 types of favor button

--- python-project-starter/react-app/src/components/FavoriteButton/index.js
+++ python-project-starter/react-app/src/components/FavoriteButton/index.js
@@ -17,12 +17,35 @@ export default function FavoriteButton() {
         if (sessionUser) dispatch(getFavoredRecipes(sessionUser.id)); //get logged user's favorites to determine which button to show
     }, [dispatch, sessionUser]);
 
+    //Onclick event handlers
     const favor = async (e) => {
         e.preventDefault();
 
         let resultErrors = await dispatch(favorRecipe(currentRecipe.id));
 
+        if (resultErrors) {
+            setErrors(resultErrors);
+        }
     };
+
+    const unfavor = async (e) => {
+        e.preventDefault();
+
+        let resultErrors = await dispatch(unfavorRecipe(currentRecipe.id));
+
+        if (resultErrors) {
+            setErrors(resultErrors);
+        }
+    }
+
+    let favorButton;
+    if (sessionUser && !favorited && sessionUser.id === currentRecipe.user_id) {
+        favorButton = <button onClick={favor}>Favorite</button>
+    }
+    else if (sessionUser && favorited && sessionUser.id === currentRecipe.user_id) {
+        favorButton = <button onClick={unfavor}>Unfavorite</button>
+    }
+
 
     return (
         <></>
