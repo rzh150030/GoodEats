c685756e118243d84da729b126bf5d977bf7779d
Richard Huang <rzhuangsg115@gmail.com>
2021-08-02 09:49:26 -0500

    import thunks into recipe detail

--- python-project-starter/react-app/src/components/RecipeDetailPage/index.js
+++ python-project-starter/react-app/src/components/RecipeDetailPage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { getRecipe, deleteRecipe } from '../../store/recipe';
+import { favorRecipe, unfavorRecipe, getFavoredRecipes } from '../../store/favorite';
 import "./RecipeDetailPage.css";
 
 export default function RecipeDetailPage() {
