a1484db24f8ff80c03b2db2a35e9961b78ab6a67
Richard Huang <rzhuangsg115@gmail.com>
2021-08-02 13:20:40 -0500

    renders recipes links in profile

--- python-project-starter/react-app/src/components/ProfilePage/index.js
+++ python-project-starter/react-app/src/components/ProfilePage/index.js
@@ -16,15 +16,24 @@ export default function ProfilePage() {
     }, [dispatch])
 
     return (
-        <div>
+        <div className="user-profile-page">
             <div>
                 <span>Your Recipes</span>
                 <span>Favorite Recipes</span>
             </div>
-            <div>
-                <div>
+            <div className="profile-items">
+                <div className="profile-recipes-container">
                     {userOwnRecipes && userOwnRecipes.map(recipe => (
-                        <div>
+                        <div className="user-recipe-containers" key={recipe.id}>
+                            <NavLink to={`/recipe/detail/${recipe.id}`}>
+                                {recipe.name}
+                            </NavLink>
+                        </div>
+                    ))}
+                </div>
+                <div className="profile-recipes-container">
+                    {userFavorites && userFavorites.map(recipe => (
+                        <div className="user-recipe-containers" key={recipe.id}>
                             <NavLink to={`/recipe/detail/${recipe.id}`}>
                                 {recipe.name}
                             </NavLink>
