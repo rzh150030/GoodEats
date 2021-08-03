b8a53a4327f8220fa6eadc3cd972eb1def13fb0b
Richard Huang <rzhuangsg115@gmail.com>
2021-08-03 16:20:50 -0500

    set up some elements in comment form

---
+++ python-project-starter/react-app/src/components/CommentFormModal/CommentForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { createReview, updateReview } from "../../store/review";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function CommentForm() {
+    const dispatch = useDispatch();
+    const [comment, setComment] = useState("");
+    const [errors, setErrors] = useState([]);
+    let updating = false;
+
+    const handleSubmit = (e) => {
+
+    };
+
+    let commentInput;
+    
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <ul>
+                {errors && errors.map((err, i) => <li key={i}>{err}</li>)}
+            </ul>
+            <label>Comment:</label>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
