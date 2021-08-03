6bf33c021f36a9408b56dbb1d101f077f8e76341
Richard Huang <rzhuangsg115@gmail.com>
2021-08-03 13:00:47 -0500

    create const types in thunk

---
+++ python-project-starter/react-app/src/store/review.js
@@ -0,0 +1,6 @@
+const CREATE_REVIEW = "review/createReview";
+const LOAD_REVIEWS = "review/loadReviews";
+const UPDATE_REVIEW = "review/updateReview";
+const DELETE_REVIEW = "review/deleteReview";
+
+
