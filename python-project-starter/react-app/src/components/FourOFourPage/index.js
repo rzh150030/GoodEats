f7d963be6fd6fd0f8c3c796bd31d5d38ab390fce
Richard Huang <rzhuangsg115@gmail.com>
2021-08-04 15:51:57 -0500

    create 404 page component

---
+++ python-project-starter/react-app/src/components/FourOFourPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+export default function FourOFourPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
