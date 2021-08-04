9c5a483f6f6065a8dbeddc65f2a093d3d1afdd3c
Richard Huang <rzhuangsg115@gmail.com>
2021-08-04 16:48:45 -0500

    style page content into column

--- python-project-starter/react-app/src/components/FourOFourPage/index.js
+++ python-project-starter/react-app/src/components/FourOFourPage/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import altonBrown from "../../images/altonbrownimage.jpg";
+import "./FourOFourPage.css";
 
 export default function FourOFourPage() {
     return (
         <div id="not-found-page">
             <img src={altonBrown} alt="Alton Brown"/>
-            <span>Page not found</span>
-            <NavLink to="/">Click here to return to home</NavLink>
+            <div id="not-found-page-content">
+                <span id="page-message">Page not found</span>
+                <NavLink to="/" id="">Click here to return to home</NavLink>
+            </div>
         </div>
     )
 }
