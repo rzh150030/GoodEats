0921f5aded198c120cf8ae00d6d129e4f627e948
Richard Huang <rzhuangsg115@gmail.com>
2021-08-04 11:54:37 -0500

    remove a trailing {} from imports

--- python-project-starter/react-app/src/components/EditRecipe/index.js
+++ python-project-starter/react-app/src/components/EditRecipe/index.js
@@ -1,4 +1,4 @@
-import React, {} from "react";
+import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import RecipeForm from "../RecipeForm";
