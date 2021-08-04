774522611a9203b314903c7a880d3433efcf6631
Richard Huang <rzhuangsg115@gmail.com>
2021-08-04 16:24:28 -0500

    add alto brown to 404 page

--- python-project-starter/react-app/src/components/FourOFourPage/index.js
+++ python-project-starter/react-app/src/components/FourOFourPage/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import altonBrown from "../../images/altonbrownimage.jpg";
 
 export default function FourOFourPage() {
     return (
         <div>
-            <h1>NOOOOOOOOOOOOO</h1>
+            <img src={altonBrown} alt="Alton Brown"/>
         </div>
     )
 }
