abf451dc7c24a2a9252a519555b8ca9cff1966ea
Richard Huang <rzhuangsg115@gmail.com>
2021-08-04 16:29:40 -0500

    add link to home page

--- python-project-starter/react-app/src/components/FourOFourPage/index.js
+++ python-project-starter/react-app/src/components/FourOFourPage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import altonBrown from "../../images/altonbrownimage.jpg";
 
 export default function FourOFourPage() {
     return (
-        <div>
+        <div id="not-found-page">
             <img src={altonBrown} alt="Alton Brown"/>
+            <span>Page not found</span>
+            <NavLink to="/">Click here to return to home</NavLink>
         </div>
     )
 }
