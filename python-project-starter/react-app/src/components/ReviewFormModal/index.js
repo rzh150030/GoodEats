400e0d32282be7afdddc2f6355acd13f7751363b
Richard Huang <rzhuangsg115@gmail.com>
2021-08-05 15:33:49 -0500

    change comment form modal to review form modal

--- python-project-starter/react-app/src/components/ReviewFormModal/index.js
+++ python-project-starter/react-app/src/components/ReviewFormModal/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import ReviewForm from './ReviewForm';
 
-export default function CommentFormModal() {
+export default function ReviewFormModal() {
     const [showModal, setShowModal] = useState(false);
 
 
