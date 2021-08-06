834cb55dc6abb764582edf0d25dbd2832a8a46e2
Richard Huang <rzhuangsg115@gmail.com>
2021-08-06 17:54:35 -0500

    styled write review button

--- python-project-starter/react-app/src/components/ReviewFormModal/index.js
+++ python-project-starter/react-app/src/components/ReviewFormModal/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import ReviewForm from '../ReviewForm';
+import "./ReviewFormModal.css";
 
 export default function ReviewFormModal({updateState, initReview, setUpdateState}) {
     const [showModal, setShowModal] = useState(false);
@@ -15,7 +16,7 @@ export default function ReviewFormModal({updateState, initReview, setUpdateState
 
     return (
         <>
-            <button onClick={openModal}>Write a review</button>
+            <button onClick={openModal} className="create-review-button">Write a review</button>
             {showModal && (
                 <Modal onClose={closeModal}>
                     <ReviewForm updateState={updateState} initReview={initReview}/>
