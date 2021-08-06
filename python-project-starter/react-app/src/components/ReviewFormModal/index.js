30ae7046b32e67b326a144b4630d27893c3fe0c1
Richard Huang <rzhuangsg115@gmail.com>
2021-08-06 12:20:19 -0500

    refactor components and file structure

--- python-project-starter/react-app/src/components/ReviewFormModal/index.js
+++ python-project-starter/react-app/src/components/ReviewFormModal/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
-import ReviewForm from './ReviewForm';
+import ReviewForm from '../ReviewForm';
 
 export default function ReviewFormModal() {
     const [showModal, setShowModal] = useState(false);
